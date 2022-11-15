cd951737e15e4e6971387191c921c3ccf4c6ed2f
Vivek Sharma <vivek.sharma_cs18@gla.ac.in>
2022-11-15 12:13:04 +0530

    added cached image

--- lib/view/with_category/with_category.dart
+++ lib/view/with_category/with_category.dart
@@ -1,5 +1,6 @@
 import 'dart:html';
 
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
@@ -72,10 +73,10 @@ class _WithCategoryState extends State<WithCategory> {
                               width: ScreenUtils.width * 22 / 23,
                               decoration: BoxDecoration(
                                   image: DecorationImage(
-                                      image: NetworkImage(
-                                          "https://corsanywhere.herokuapp.com/${i.imageUrl!}" ??
+                                      image: CachedNetworkImageProvider(
+                                        "https://corsanywhere.herokuapp.com/${i.imageUrl!}" ??
                                               Images.bedroom,
-                                          headers: ImageService.headers),
+                                          headers: ImageService.headers,),
                                       fit: BoxFit.cover)),
                             ),
                             Positioned(
--- lib/view/without_category/without_category.dart
+++ lib/view/without_category/without_category.dart
@@ -1,3 +1,4 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
@@ -51,7 +52,7 @@ class _WithoutCategoryState extends State<WithoutCategory> {
                     width: ScreenUtils.width * 22 / 23,
                     decoration: BoxDecoration(
                         image: DecorationImage(
-                            image: NetworkImage("https://corsanywhere.herokuapp.com/${i.imageUrl!}" ?? Images.bedroom,headers: ImageService.headers),
+                            image: CachedNetworkImageProvider("https://corsanywhere.herokuapp.com/${i.imageUrl!}" ?? Images.bedroom,headers: ImageService.headers),
                             fit: BoxFit.cover)),
                   ),
                   Positioned(
